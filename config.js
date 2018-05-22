8fef85cf0c1736acf6eb2675eb060809238eb04e
Przemysław Wróbel <przemek.wrobel@poczta.fm>
2018-05-22 23:15:48 +0200

    First

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '1043592513307-8dil4oi2umpq503kn0c1jmq88vbobi42.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET':'PwD23665W76Gk0SoVu6A_aRy',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
+
